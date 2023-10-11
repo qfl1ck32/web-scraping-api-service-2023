300b9e0e6da5687f7207ea8261766d6b1275e0b0
Rusu Andrei-Cristian <hello@andreirusu.dev>
2023-10-11 12:22:12 +0300

    Fix missing provider

--- back-end/src/scraper/scraper.controller.spec.ts
+++ back-end/src/scraper/scraper.controller.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { ScraperController } from './scraper.controller';
+import { ScraperService } from './scraper.service';
 
 describe('ScraperController', () => {
   let controller: ScraperController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [ScraperService],
       controllers: [ScraperController],
     }).compile();
 
