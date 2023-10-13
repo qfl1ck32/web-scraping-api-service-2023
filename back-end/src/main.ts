f2e924a414ab14d3a4c1756a5d2cb6146a8d4da8
Rusu Andrei-Cristian <hello@andreirusu.dev>
2023-10-13 18:19:48 +0300

    Cleanup & add cors

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -5,13 +5,13 @@ import { AppModule } from './app.module';
 import { setupSwagger } from './swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(new ValidationPipe());
 
   setupSwagger(app);
 
-  await app.listen(3000);
+  await app.listen(8080);
 }
 
 bootstrap();
